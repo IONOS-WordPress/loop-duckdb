2a53f82d18ed9054434c641cdd5ad4531f6cfa69
Lars Gersmann <lars.gersmann@ionos.com>
2025-11-13 18:03:38 +0100

    feat: add script to generate markdown output for extendify report insights

---
+++ scripts/generate-report-parts/10-extendify.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#
+^# generates markdown output for extendify related questions of the report
+# 
+
+readonly SQL="
+SELECT
+  count(DISTINCT CASE WHEN theme.id = 'extendable' THEN instance END) AS 'using extendable',
+  count(DISTINCT CASE WHEN theme.id <> 'extendable' THEN instance END) AS 'other theme'
+FROM
+  themes;
+"
+
+TITLE="How many of our customers use extendify ?"
+
+cat <<EOF
+
+# $TITLE
+
+\`\`\`mermaid
+$(echo $(ionos.loop-duckdb.exec_duckdb "$SQL" '-json') | jq -r --arg title "$TITLE" '
+    "pie showData title \($title)",
+    (.[] | to_entries[] | "  \(.key) : \(.value)")
+')
+\`\`\`
+EOF
+
